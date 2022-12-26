4fe06405a0995b895d8f59e62c25912755a08c18
unknown <john.christopher.enriquez2@gmail.com>
2022-12-26 22:25:40 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+//First task
+//This is an inline comment
+
+/* This is a 
+multi-line comment*/
+
+//Second task
+var myName;
+
+//Third task
+var a = 7;
+
+//Fourth task
+var b;
+b = 7;
+var c;
+c = b;
+
+//Fifth task
+var a = 9;
+
+//Sixth task
+var myFirstName = "John Christopher";
+var myLastName = "Enriquez";
+
+//Seventh task
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//Eigth task
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+var studlyCapVar = 10;
+var properCamelCase = "A String";
+var titleCaseOver = 9000;
+
+//Ninth task
+let catName = "Oliver";
+let catSound = "Meow!";
+
+//Tenth task
+const FCC = "freeCodeCamp";
+let fact = "is cool!";
+fact = "is awesome!";
+console.log(FCC, fact);
+
+//11th task
+const sum = 10 + 10;
+
+//12th task
+const difference = 45 - 33;
