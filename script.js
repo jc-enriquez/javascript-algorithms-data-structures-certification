ddfeafffb79c0719ae41aa27d5478cea9a04aa42
unknown <john.christopher.enriquez2@gmail.com>
2022-12-27 13:39:38 +0800

    second commit

--- script.js
+++ script.js
@@ -58,3 +58,127 @@ const sum = 10 + 10;
 
 //12th task
 const difference = 45 - 33;
+
+//13th task
+const product = 8 * 10;
+
+//14th task
+const quotient = 66 / 33;
+
+//15th task
+let myVar = 87;
+
+myVar = ++myVar;
+
+//16th task
+let myVarDecrement = 11;
+
+// Only change code below this line
+myVarDecrement = --myVarDecrement;
+
+//17th task
+const ourDecimal = 5.7;
+
+// Only change code below this line
+const myDecimal = 2.2;
+
+//18th task
+const productDecimal = 2.0 * 2.5;
+
+//19th task
+const quotientDecimal = 4.4 / 2.0; // Change this line
+
+//20th task
+const remainder = 11 % 3;
+
+//21st task
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+//22nd task
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+//23rd task
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+//24th task
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+//25th task
+const myStr = 'I am a "double quoted" string inside "double quotes".'; // Change this line
+
+//26th task
+const myStrQuotes = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//27th task
+const myStrLong = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+//28th task
+const myStrConcat = "This is the start. " + "This is the end."; // Change this line
+
+//29th task
+let newStr = "This is the first sentence. ";
+newStr += "This is the second sentence.";
+
+//30th task
+// Only change code below this line
+const myName = "John Christopher Enriquez";
+const newerStr = "My name is " + myName + " and I am well!";
+
+//31st task
+// Change code below this line
+const someAdjective = "fun";
+let sentence = "Learning to code is ";
+
+sentence += someAdjective;
+
+//32nd task
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+//33rd task
+// Setup
+let firstLetterOfLastName = "";
+const lastNameNewer = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+//34th task
+// Setup
+let newestStr = "Jello World";
+
+// Only change code below this line
+newestStr = "Hello World"; // Change this line
+// Only change code above this line
